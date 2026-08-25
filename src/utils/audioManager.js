df4837568d5b458066ccb3bd3d8616cdcab83e4a
Axadov Abdurasul <axadovabdurasul51@gmail.com>
2026-08-25 10:29:39 +0500

    Replace background music with Indila - Love Story (Fairy Version)

--- src/utils/audioManager.js
+++ src/utils/audioManager.js
@@ -16,7 +16,7 @@ export const initAudio = () => {
 
     if (!bgMusicAudio) {
         // We use the file provided by the user in public/sounds/
-        bgMusicAudio = new Audio('/sounds/cfl_turningpages-belem-breeze-487596.ogg');
+        bgMusicAudio = new Audio('/sounds/love_story_fairy.mp3');
         bgMusicAudio.preload = 'auto'; // Force browser to fetch data immediately
         bgMusicAudio.loop = true;
         bgMusicAudio.volume = 0.3; // Default volume for background cozy music
